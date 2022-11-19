6a3d5d3720577ad6073a9815ef1266290fed49ed
Masaaki618 <masaaki_618@icloud.com>
2022-11-19 10:38:35 +0900

    #4 add: ベースとなるボタンコンポーネントを作成

---
+++ components/atoms/buttons/BaseButton.tsx
@@ -0,0 +1,17 @@
+import {Button} from '@chakra-ui/react'
+
+type Props = {
+  backgroundColor: string
+  fontColor: string
+  children: React.ReactNode
+  borderColor: string
+}
+
+const BaseButton = ({backgroundColor, fontColor, borderColor, children}: Props) => {
+  return (
+    <Button background={backgroundColor} color={fontColor} rounded="full" border='1px' borderColor={borderColor}
+            py={'6'}>{children}</Button>
+  );
+};
+
+export default BaseButton;

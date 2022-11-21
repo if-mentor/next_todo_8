cad8f87d5d3f98b363fda8575cc385668338d871
Masaaki618 <masaaki_618@icloud.com>
2022-11-21 21:56:36 +0900

    #4 refactor: デフォルトで引数を取るように変更

--- pages/confirmationButton.tsx
+++ pages/confirmationButton.tsx
@@ -1,33 +1,23 @@
-import {Center} from '@chakra-ui/react'
-import NotStarted from "../components/atoms/buttons/notStarted";
-import Doing from "../components/atoms/buttons/doing";
-import Done from "../components/atoms/buttons/done";
-import DeleteAll from "../components/atoms/buttons/deleteAll";
-import RestoreAll from "../components/atoms/buttons/restoreAll";
-import Back from "../components/atoms/buttons/back";
-import Reset from "../components/atoms/buttons/reset";
-import Draft from "../components/atoms/buttons/draft";
-import Create from "../components/atoms/buttons/create";
-import Update from "../components/atoms/buttons/update";
-import Comment from "../components/atoms/buttons/comment";
+import { Center } from "@chakra-ui/react";
+import BaseButton from "../components/atoms/buttons/BaseButton";
 
 const ConfirmationButton = () => {
   return (
     <>
-      <Center mt={'4'}>
-        <NotStarted/>
-        <Doing/>
-        <Done/>
-        <DeleteAll/>
-        <RestoreAll/>
-        <Back/>
-        <Reset/>
+      <Center mt={"4"}>
+        <BaseButton backgroundColor={"green.50"}>NOT STARTED</BaseButton>
+        <BaseButton backgroundColor={"green.500"} fontColor={"white"}>DOING</BaseButton>
+        <BaseButton backgroundColor={"green.200"}>DONE</BaseButton>
+        <BaseButton backgroundColor={"red.300"} fontColor={'white'} borderColor={'white'}>Delete all</BaseButton>
+        <BaseButton backgroundColor={"blue.300"} fontColor={'white'} borderColor={'white'}>Restore All</BaseButton>
+        <BaseButton backgroundColor={"green.200"}>Back</BaseButton>
+        <BaseButton backgroundColor={'blackAlpha.400'}>RESET</BaseButton>
       </Center>
-      <Center mt={'9'}>
-        <Draft/>
-        <Create/>
-        <Update/>
-        <Comment/>
+      <Center mt={"9"}>
+        <BaseButton backgroundColor={'pink.100'}>DRAFT</BaseButton>
+        <BaseButton backgroundColor={"green.500"} fontColor={"white"}>CREATE</BaseButton>
+        <BaseButton backgroundColor={"green.500"} fontColor={"white"}>UPDATE</BaseButton>
+        <BaseButton backgroundColor={"green.500"} fontColor={"white"}>Comment</BaseButton>
       </Center>
     </>
   );

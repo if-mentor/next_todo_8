8d291b8dc9f964f21af44bdaf57afb8857915fb0
maki <murasame.asa.73@gmail.com>
2022-11-22 21:06:01 +0900

    #7 add: セレクトボックス（PriorityButton）コンポーネントの作成

--- components/atoms/selectbox/PriorityButtonBox.tsx
+++ components/atoms/selectbox/PriorityButtonBox.tsx
@@ -1,2 +1,22 @@
 import {FC} from 'react'
 import { Select } from '@chakra-ui/react'
+
+
+const PriorityButtonBox: React.FC= () => {
+  return (
+    <>
+      <Select
+        height="40px"
+        width="112px"
+        borderColor="#E53E3E"
+        fontSize="16px"
+      >
+        <option value='High'>High</option>
+        <option value='Middle'>Middle</option>
+        <option value='Low'>Low</option>
+      </Select>
+    </>
+  )
+}
+
+export default PriorityButtonBox;

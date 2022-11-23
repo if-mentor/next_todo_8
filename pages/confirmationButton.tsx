fc4b2fae081c631cff9b9298b2d3db9e97c1d5cd
Masaaki618 <masaaki_618@icloud.com>
2022-11-23 21:56:53 +0900

    #4 refactor: ボタン一覧ページの修正

--- pages/confirmationButton.tsx
+++ pages/confirmationButton.tsx
@@ -6,6 +6,7 @@ import { DeleteAllButton } from "../components/atoms/buttons/DeleteAllButton";
 import { DoingButton } from "../components/atoms/buttons/DoingButton";
 import { DoneButton } from "../components/atoms/buttons/DoneButton";
 import { DraftButton } from "../components/atoms/buttons/DraftButton";
+import { GoogleLoginButton } from "../components/atoms/buttons/GoogleLoginButton";
 import { LoginButton } from "../components/atoms/buttons/LoginButton";
 import { NotStartedButto } from "../components/atoms/buttons/NotStartedButton";
 import { ResetButton } from "../components/atoms/buttons/ResetButton";
@@ -13,6 +14,8 @@ import { RestoreAllButton } from "../components/atoms/buttons/RestoreAllButton";
 import { SignUpButton } from "../components/atoms/buttons/SignUpButton";
 import { TopButton } from "../components/atoms/buttons/TopButton";
 import { UpdateButton } from "../components/atoms/buttons/UpdateButton";
+import { DeleteButton } from "../components/atoms/buttons/DeleteButton";
+import {RestoreButton } from "../components/atoms/buttons/RestoreButton";
 
 const ConfirmationButton = () => {
   return (
@@ -25,6 +28,8 @@ const ConfirmationButton = () => {
         <RestoreAllButton />
         <BackButton />
         <ResetButton />
+        <DeleteButton />
+        <RestoreButton />
       </Center>
       <Center mt={"9"}>
         <DraftButton />
@@ -34,6 +39,7 @@ const ConfirmationButton = () => {
         <SignUpButton />
         <LoginButton />
         <TopButton />
+        <GoogleLoginButton />
       </Center>
     </>
   );

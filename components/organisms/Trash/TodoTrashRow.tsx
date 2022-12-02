62d7536c30c2c2915eabf1df98c7331d382a3628
natsuki <06un21@gmail.com>
2022-12-02 12:12:54 +0900

    #19 add:todoTrash作成

---
+++ components/organisms/Trash/TodoTrashRow.tsx
@@ -0,0 +1,62 @@
+import React, { FC } from "react";
+import {
+  TableColumnHeaderProps,
+  TableBodyProps,
+  Tbody,
+  Td,
+  Tr,
+} from "@chakra-ui/react";
+import PriorityButtonBox from "../../atoms/selectbox/PriorityButtonBox";
+import { RestoreButton } from "../../atoms/buttons/RestoreButton";
+import { DeleteButton } from "../../atoms/buttons/DeleteButton";
+
+type Props = {
+  task: string;
+  status: React.ReactNode;
+  create: any;
+};
+
+const TodoTrashRow: FC<Props> = (props) => {
+  const { task, status, create } = props;
+
+  const TrashTbody = (props: TableBodyProps) => {
+    return (
+      <Tbody h="56px" pb="-1" borderColor="rgba(0, 0, 0, 0.25)" {...props} />
+    );
+  };
+
+  const TrashWhiteCell = (props: TableColumnHeaderProps) => {
+    return (
+      <Td
+        maxW="174px"
+        minW="174px"
+        fontFamily="Roboto"
+        fontSize="16px"
+        color="BlackAlpha.800"
+        p="12px 10px"
+        {...props}
+      />
+    );
+  };
+  return (
+    <>
+      <TrashTbody>
+        <Tr>
+          <TrashWhiteCell maxW="384px" minW="384px" >
+            {task}
+          </TrashWhiteCell>
+          <TrashWhiteCell>{status}</TrashWhiteCell>
+          <TrashWhiteCell>
+            <PriorityButtonBox />
+          </TrashWhiteCell>
+          <TrashWhiteCell>{create}</TrashWhiteCell>
+          <TrashWhiteCell>
+            <RestoreButton />
+            <DeleteButton />
+          </TrashWhiteCell>
+        </Tr>
+      </TrashTbody>
+    </>
+  );
+};
+export default TodoTrashRow;

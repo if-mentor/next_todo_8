45c5e5af8ccc3d5362eb4b64b330b57169e3623f
maki <murasame.asa.73@gmail.com>
2022-12-06 16:02:49 +0900

    #20 fix: ToppageListHeadおよび ListLineのmaxWidthを修正

--- components/organisms/ToppageListHead.tsx
+++ components/organisms/ToppageListHead.tsx
@@ -9,9 +9,9 @@ const ToppageListHead = () => {
           <Th textTransform="none" fontSize="24px" fontWeight="700" maxW={"380px"} minW={"380px"} p={"16px 10px"}>Task</Th>
           <Th textTransform="none" fontSize="24px" fontWeight="700" maxW={"140px"} minW={"140px"} p={"16px 10px"}><Center>Status</Center></Th>
           <Th textTransform="none" fontSize="24px" fontWeight="700" maxW={"140px"} minW={"140px"} p={"16px 10px"}><Center>Priority</Center></Th>
-          <Th textTransform="none" fontSize="24px" fontWeight="700" maxW={"140px"} minW={"140px"} p={"16px 10px"}><Center>Create</Center></Th>
-          <Th textTransform="none" fontSize="24px" fontWeight="700" maxW={"140px"} minW={"140px"} p={"16px 10px"}><Center>Update</Center></Th>
-          <Th textTransform="none" fontSize="24px" fontWeight="700" maxW={"115px"} minW={"115px"} p={"16px 10px"}><Center>Action</Center></Th>
+          <Th textTransform="none" fontSize="24px" fontWeight="700" maxW={"150px"} minW={"150px"} p={"16px 0px"}><Center>Create</Center></Th>
+          <Th textTransform="none" fontSize="24px" fontWeight="700" maxW={"150px"} minW={"150px"} p={"16px 0px"}><Center>Update</Center></Th>
+          <Th textTransform="none" fontSize="24px" fontWeight="700" maxW={"120px"} minW={"120px"} p={"16px 10px"}><Center>Action</Center></Th>
         </Tr>
       </Thead>
     </>

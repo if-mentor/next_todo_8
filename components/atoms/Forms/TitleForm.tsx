d960bd787e7991442c654a6e2673f5c7022da6dd
maki <murasame.asa.73@gmail.com>
2022-12-13 22:29:32 +0900

    #28 add:titleとdetailをrecoilで管理、onChangeでinput内容を監視

--- components/atoms/Forms/TitleForm.tsx
+++ components/atoms/Forms/TitleForm.tsx
@@ -1,12 +1,17 @@
 import  TextArea  from "./accessories/TextArea";
+import { useRecoilState } from 'recoil'
+import { todoTitleState } from "../../../state/todoTitleState";
 
 const TitleForm = () => {
+  // titleの値をrecoilで管理
+  const [todoTitle, setTodoTitle] = useRecoilState(todoTitleState);
   return (
     <>
       <TextArea
         h="104px"
         fontSize={24}
         placeholder="Text"
+        onChange={(e) => setTodoTitle(e.target.value)}
       />
     </>
   );

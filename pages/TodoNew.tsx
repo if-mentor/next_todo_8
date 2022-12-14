b149dca2842dea93adb82a67c6abce953f51ad02
shtk928 <s.takanori0119@icloud.com>
2022-12-14 21:23:48 +0900

    code fiix

--- pages/TodoNew.tsx
+++ pages/TodoNew.tsx
@@ -1,5 +1,6 @@
-import { Box, FormControl, FormLabel, Radio, RadioGroup, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
+import { Box, FormControl, FormLabel, Radio, RadioGroup, Stack, Text } from '@chakra-ui/react'
+import { Header } from '../components/organisms/Header'
 import { BackButton } from '../components/atoms/buttons/BackButton'
 import { CreateButton } from '../components/atoms/buttons/CreateButton'
 import { DraftButton } from '../components/atoms/buttons/DraftButton'
@@ -9,18 +10,25 @@ import TitleForm from '../components/atoms/Forms/TitleForm'
 const TodoNew = () => {
   return (
     <Box>
-      {/* headerコンポーネントをここに入れる */}
-      <Text>ヘッダー</Text>
-      <Box display={'flex'} justifyContent={'space-between'} marginLeft={100} marginRight={220}>
-        <Text fontSize={28} fontWeight={700}>NEW TODO</Text>
+      <Header />
+      <Box w='1080px' m='36px auto 0' display={'flex'} justifyContent={'space-between'}>
+        <Text fontSize={28} fontWeight={700}>
+          NEW TODO
+        </Text>
         <BackButton />
       </Box>
-      <FormControl marginLeft={100} marginRight={100}>
-        <FormLabel fontSize={24} fontWeight={700} display={'flex'}>TITLE</FormLabel>
+      <FormControl w='1080px' m='0 auto'>
+        <FormLabel fontSize={24} fontWeight={700}>
+          TITLE
+        </FormLabel>
         <TitleForm />
-        <FormLabel fontSize={24} fontWeight={700}>DETAIL</FormLabel>
+        <FormLabel fontSize={24} fontWeight={700}>
+          DETAIL
+        </FormLabel>
         <TextBox />
-        <Text fontSize={24} fontWeight={700}>PRIORITY</Text>
+        <Text fontSize={24} fontWeight={700}>
+          PRIORITY
+        </Text>
         <RadioGroup fontSize={24} fontWeight={700} display={'flex'} justifyContent={'flex-start'}>
           <Stack direction={'row'}>
             <Radio>High</Radio>
@@ -28,13 +36,13 @@ const TodoNew = () => {
             <Radio>Low</Radio>
           </Stack>
         </RadioGroup>
-        <Box marginRight={320} display={'flex'} justifyContent={'flex-end'}>
+        <Box display={'flex'} justifyContent={'flex-end'}>
           <DraftButton />
           <CreateButton />
         </Box>
       </FormControl>
     </Box>
-    )
-}
+  );
+};
 
-export default TodoNew
+export default TodoNew;

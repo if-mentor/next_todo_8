75f3b3b7b5a9bcacdbcc70565fb2eedd1ba24054
maki <murasame.asa.73@gmail.com>
2022-12-21 15:56:21 +0900

    #28 add: firebaseにデータを送れるようにコードを追加

--- components/atoms/buttons/CreateButton.tsx
+++ components/atoms/buttons/CreateButton.tsx
@@ -4,7 +4,7 @@ import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 import { uuidv4 } from '@firebase/util'
 import { Button } from "@chakra-ui/react";
 
-import { auth, db } from "../../../firebase";
+import { db } from "../../../firebase";
 import { todoTitleState } from "../../../state/todoTitleState";
 import { todoDetailState } from "../../../state/todoDetailState";
 import { todoPriorityValueState } from "../../../state/todoPriorityValueState";
@@ -21,26 +21,20 @@ export const CreateButton = () => {
 
   // CREATEボタンを押したときに、firebaseにデータを送信するための関数
   const createTodo =async (e:any) => {
-    // authがnullの可能性があります という文言を消すための関数だが、今の段階で入れると式がreturnしてしまう
-    // if(auth.currentUser === null){
-    //   return
-    // } 
     e.preventDefault();
     // コレクションは"users"でその中にサブコレクションとして"todos"を作る
-    const docRef = doc(db, "users", auth.currentUser.uid, "todos", docId);
+    const docRef = doc(db, "todos", docId);
     const data = {
       text:todoTitle,
       detail:todoDetail,
-      userId:auth.currentUser.uid,
-      id:docId,
       status:"NOT STARTED",
       priority:priorityValue,
       createdAt: serverTimestamp(),
       updateAt: serverTimestamp(),
     };
     await setDoc(docRef ,data);
-    // 送信したらTopページに戻る（Topページがまだマージされていないので仮に"/"に遷移）
-    await router.push("/");
+    // 送信したらTopページに戻る
+    await router.push("/todoTop");
   }
   return (
     <Button

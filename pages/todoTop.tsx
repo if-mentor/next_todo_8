6300aaccbdbc7b231df66d64711a178ac5eed9da
Masaaki618 <masaaki_618@icloud.com>
2022-12-22 19:07:36 +0900

    #15 add: firebaseの一覧データを表示できる機能の追加

--- pages/todoTop.tsx
+++ pages/todoTop.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import { Box, TableContainer, Table, Flex, Text, Spacer, HStack} from "@chakra-ui/react";
-import { DoingButton } from "../components/atoms/buttons/DoingButton";
-import { DoneButton } from "../components/atoms/buttons/DoneButton";
-import { NotStartedButton } from "../components/atoms/buttons/NotStartedButton";
-import TemporaryToppageListHead from "../components/temporaryComponents/TemporaryToppageListHead";
-import TemporaryToppageListLine from "../components/temporaryComponents/TemporaryToppageListLine";
+import React, { useEffect, useState } from 'react';
+import {
+  Box,
+  TableContainer,
+  Table,
+  Flex,
+  Text,
+  Spacer,
+  HStack,
+} from '@chakra-ui/react';
+import { DoingButton } from '../components/atoms/buttons/DoingButton';
+import TemporaryToppageListHead from '../components/temporaryComponents/TemporaryToppageListHead';
+import TemporaryToppageListLine from '../components/temporaryComponents/TemporaryToppageListLine';
 import TrashIcon from '../components/atoms/icons/TrashIcon';
 import DraftIcon from '../components/atoms/icons/DraftIcon';
 import TextAreaBox from '../components/atoms/selectbox/TextAreaBox';
@@ -14,14 +20,45 @@ import FooterNumberButton from '../components/atoms/footerButton/FooterNumberBut
 import FooterNumberPlusButton from '../components/atoms/footerButton/FooterNumberPlusButton';
 import FooterNumberMinusButton from '../components/atoms/footerButton/FooterNumberMinusButton';
 import { Header } from '../components/organisms/Header';
+import { db } from '../firebase';
+import {
+  collection,
+  CollectionReference,
+  Timestamp,
+  onSnapshot,
+} from 'firebase/firestore';
+import { format } from 'date-fns';
+
+type Todos = {
+  id: number;
+  createdAt: Timestamp;
+  detail: string;
+  priority: string;
+  status: string;
+  text: string;
+  updateAt: Timestamp;
+};
 
 const todoTop = () => {
+  const [todos, setTodos] = useState<Array<Todos>>([]);
+
+  useEffect(() => {
+    //データの取得
+    const postData = collection(db, 'todos') as CollectionReference<Todos>;
+
+    //リアルタイムでデータを取得する。
+    onSnapshot(postData, (todo) => {
+      setTodos(todo.docs.map((doc) => ({ ...doc.data() })));
+    });
+  }, []);
   return (
     <>
       <Header />
-      <Flex  justify='center' align='center'>
+      <Flex justify="center" align="center">
         <Flex w="1080px" m="16px 0px">
-          <Text fontSize="28px" fontWeight="700">TODO LIST</Text>
+          <Text fontSize="28px" fontWeight="700">
+            TODO LIST
+          </Text>
           <Spacer />
           <Box pr="16px">
             <TrashIcon />
@@ -31,7 +68,7 @@ const todoTop = () => {
           </Box>
         </Flex>
       </Flex>
-      <Flex justify='center' align='center'>
+      <Flex justify="center" align="center">
         <Box minW="1080px" w="1080px">
           <HStack pb="33px">
             <Box pr="24px">
@@ -40,11 +77,11 @@ const todoTop = () => {
             </Box>
             <Box pr="24px">
               <Text fontSize="18px">STATUS</Text>
-              <TextAreaBox value1="NOT STARTED" value2="DOING" value3="DONE"/>
+              <TextAreaBox value1="NOT STARTED" value2="DOING" value3="DONE" />
             </Box>
             <Box pr="24px">
               <Text fontSize="18px">PROPRITY</Text>
-              <TextAreaBox value1="High" value2="Middle" value3="Low"/>
+              <TextAreaBox value1="High" value2="Middle" value3="Low" />
             </Box>
             <Flex h="67px" alignItems="flex-end">
               <ResetButton />
@@ -53,54 +90,32 @@ const todoTop = () => {
         </Box>
       </Flex>
 
-      <Flex  justify='center' align='center'>
+      <Flex justify="center" align="center">
         <TableContainer w="1080px">
           <Table variant="simple">
-
             {/* #20-Add-Toppage-List-Lineがマージされたら書き換える */}
             <TemporaryToppageListHead />
-            <TemporaryToppageListLine
-              status={<NotStartedButton />}
-              task="Github上に静的サイトをホスティングする"
-              create="2022-11-11 12:12"
-              update="2022-12-11 12:12"
-            />
             <TemporaryToppageListLine
               status={<DoingButton />}
               task="ReactでTodoサイトを作成する"
               create="2022-10-11 12:12"
               update="2022-12-14 12:12"
             />
-            <TemporaryToppageListLine
-              status={<DoneButton />}
-              task="Firestore Hostingを学習する"
-              create="2022-11-15 12:12"
-              update="2022-12-24 12:00"
-            />
-            <TemporaryToppageListLine
-              status={<NotStartedButton />}
-              task="こんにちは"
-              create="2022-11-11 12:12"
-              update="2022-12-11 12:12"
-            />
-            <TemporaryToppageListLine
-              status={<NotStartedButton />}
-              task="はろー"
-              create="2022-11-11 12:12"
-              update="2022-12-11 12:12"
-            />
-            <TemporaryToppageListLine
-              status={<NotStartedButton />}
-              task="だーじゃーはお"
-              create="2022-11-11 12:12"
-              update="2022-12-11 12:12"
-            />
-            {/* ここまで */}
 
+            {todos.map((todo) => (
+              <TemporaryToppageListLine
+                key={todo.id}
+                status={<DoingButton />} //仮でボタン
+                task={todo.text}
+                create={format(todo.createdAt.toDate(), 'yyyy-MM-dd HH:mm')}
+                update={format(todo.updateAt.toDate(), 'yyyy-MM-dd HH:mm')}
+              />
+            ))}
+            {/* ここまで */}
           </Table>
         </TableContainer>
       </Flex>
-      <Flex  justify='center' align='center'>
+      <Flex justify="center" align="center">
         <FooterNumberMinusButton />
         <FooterNumberButton footerNum={1} />
         <FooterNumberButton footerNum={2} />
@@ -110,7 +125,7 @@ const todoTop = () => {
         <FooterNumberPlusButton />
       </Flex>
     </>
-  )
-}
+  );
+};
 
-export default todoTop
+export default todoTop;

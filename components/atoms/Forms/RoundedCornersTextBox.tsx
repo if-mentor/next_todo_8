b53fbde3603e46f803ede2e48e083031fcb4fb7d
Masaaki618 <masaaki_618@icloud.com>
2022-12-26 19:36:23 +0900

    #46 add:サインアップ機能の実装

--- components/atoms/Forms/RoundedCornersTextBox.tsx
+++ components/atoms/Forms/RoundedCornersTextBox.tsx
@@ -1,12 +1,20 @@
 import InputBox from "./accessories/InputBox";
+import {FC} from "react";
 
-const RoundedCornersTextBox = () => {
+type Props = {
+  inputText: string
+  changeInputText: (Event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const RoundedCornersTextBox: FC<Props> = ({inputText, changeInputText}) => {
   return (
     <InputBox
       w="540px"
       h="57px"
       background={"#F0FCFF"}
       borderRadius={'40px'}
+      value={inputText}
+      onChange={changeInputText}
     />
   );
 };

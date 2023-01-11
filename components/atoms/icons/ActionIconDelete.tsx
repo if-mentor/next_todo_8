3965e8ffeba3a094aca17c14eed462ee65acf293
maki <murasame.asa.73@gmail.com>
2023-01-11 18:36:40 +0900

    #31 add: topページの削除ボタンの機能実装

--- components/atoms/icons/ActionIconDelete.tsx
+++ components/atoms/icons/ActionIconDelete.tsx
@@ -1,13 +1,29 @@
 import { Icon } from '@chakra-ui/react'
-import React from 'react'
+import { collection, doc, Firestore, onSnapshot, updateDoc } from 'firebase/firestore'
+import React, { useState } from 'react'
 import {  BsTrash } from 'react-icons/bs'
+import { useRecoilState } from 'recoil'
+import { db } from '../../../firebase'
+import { todoTrashState } from '../../../state/todoTrashState'
 
 const ActionIconDelete = () => {
+  // const [trashState, setTrashState] = useRecoilState(todoTrashState);
+  const handleTrashTodo = () => {
+    // idの取得方法が判明したら変更
+    const docEdit = doc(db, "todos", "8a776cd8-9565-4cc0-8f92-f0d6a18a0eac");
+    updateDoc(docEdit, {
+      trash: true
+    })
+    // setTrashState(true);
+  }
+  
   return (
     <Icon
       as={BsTrash}
       h='28px'
       w='12px'
+      cursor="pointer"
+      onClick={handleTrashTodo}
     />
   )
 }
--- components/organisms/ToppageListLine.tsx
+++ components/organisms/ToppageListLine.tsx
@@ -7,21 +7,24 @@ import ActionIconDelete from '../atoms/icons/ActionIconDelete'
 type Props = {
   task: string,
   status:any,
+  priority: string
   create: any,
   update: any,
+  trash: boolean,
+  id: string,
 }
 
-const ToppageListLine = ({task, status, create, update}:Props) => {
+const ToppageListLine = ({task, status, priority, create, update,trash,id}:Props) => {
   return (
     <>
       <Tbody>
         <Tr>
           <Td fontSize="16px" fontWeight="700" p={"16px 12px"}>{task}</Td>
           <Td fontSize="16px" fontWeight="700" p={"16px 10px"}><Center>{status}</Center></Td>
-          <Td fontSize="16px" fontWeight="700" p={"16px 10px"}><Center><PriorityButtonBox /></Center></Td>
+          <Td fontSize="16px" fontWeight="700" p={"16px 10px"}><Center><PriorityButtonBox priority={priority}/></Center></Td>
           <Td fontSize="14px" fontWeight="700" p={"16px 0px"}><Center>{create}</Center></Td>
           <Td fontSize="14px" fontWeight="700" p={"16px 0px"}><Center>{update}</Center></Td>
-          <Td fontSize="16px" fontWeight="700" p={"16px 10px"}><Center><ActionIconEdit />　<ActionIconDelete></ActionIconDelete></Center></Td>
+          <Td fontSize="16px" fontWeight="700" p={"16px 10px"}><Center><ActionIconEdit/>　<ActionIconDelete /></Center></Td>
         </Tr>
       </Tbody>
     </>

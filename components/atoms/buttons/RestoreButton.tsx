aecaadb031179cade358d3b959163fc420c87b05
maki <murasame.asa.73@gmail.com>
2023-01-11 18:38:26 +0900

    #31 add: restoreボタンの機能実装

--- components/atoms/buttons/RestoreButton.tsx
+++ components/atoms/buttons/RestoreButton.tsx
@@ -1,6 +1,18 @@
 import { Button } from "@chakra-ui/react";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../../firebase";
 
 export const RestoreButton = () => {
+
+  const handleRestoreTodo = () => {
+    // idの取得方法が判明したら変更
+    const docEdit = doc(db, "todos", "8a776cd8-9565-4cc0-8f92-f0d6a18a0eac");
+    
+    updateDoc(docEdit, {
+      trash: false
+    })
+  }
+
   return (
     <Button
       _hover={{ bg: "#40D2F1" }}
@@ -11,6 +23,7 @@ export const RestoreButton = () => {
       w={"80px"}
       fontSize={"18px"}
       fontWeight={'bold'}
+      onClick={handleRestoreTodo}
     >
       Restore
     </Button>

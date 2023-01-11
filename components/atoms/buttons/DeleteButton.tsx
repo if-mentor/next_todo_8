7a6c31bc93306dc9cba9e52cfdf25edef7830daa
maki <murasame.asa.73@gmail.com>
2023-01-11 18:37:45 +0900

    #31 add: deleteボタンの機能実装

--- components/atoms/buttons/DeleteButton.tsx
+++ components/atoms/buttons/DeleteButton.tsx
@@ -1,6 +1,16 @@
 import { Button } from "@chakra-ui/react";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "../../../firebase";
+
+// idの取得方法が判明したら変更↓
+const id ="daaxwBhgR5yWyZ9l1bKs"
 
 export const DeleteButton= () => {
+  // firebaseから削除する関数
+  const handleDeleteTodo = (e:any) => {
+    deleteDoc(doc(db, "todos",e));
+  };
+  
   return (
     <Button
       _hover={{ bg: "#E28F84" }}
@@ -11,6 +21,7 @@ export const DeleteButton= () => {
       w={"80px"}
       fontSize={"18px"}
       fontWeight={'bold'}
+      onClick={() => handleDeleteTodo(id)}
     >
       Delete
     </Button>

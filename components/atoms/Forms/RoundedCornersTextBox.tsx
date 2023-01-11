144ce3a5360be9d6b5278992a2868f1c0df8084b
BugLizard <rosikokko@gmail.com>
2023-01-11 21:37:58 +0900

    fix:エラー修正

--- components/atoms/Forms/RoundedCornersTextBox.tsx
+++ components/atoms/Forms/RoundedCornersTextBox.tsx
@@ -1,19 +1,23 @@
+import { FC } from "react";
 import InputBox from "./accessories/InputBox";
-import {FC} from "react";
 
 type Props = {
-  inputText: string
+  inputText: string;
   changeInputText: (Event: React.ChangeEvent<HTMLInputElement>) => void;
-  type?: string
-}
+  type?: string;
+};
 
-const RoundedCornersTextBox: FC<Props> = ({inputText, changeInputText, type}) => {
+const RoundedCornersTextBox: FC<Props> = ({
+  inputText,
+  changeInputText,
+  type,
+}) => {
   return (
     <InputBox
       w="540px"
       h="57px"
       background={"#F0FCFF"}
-      borderRadius={'40px'}
+      borderRadius={"40px"}
       value={inputText}
       onChange={changeInputText}
       type={type}

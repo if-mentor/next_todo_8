605b6d1fdfd9a9f564072feca22c8cb7249de4dc
maki <murasame.asa.73@gmail.com>
2023-01-11 16:46:46 +0900

    #31 add: todoTrashページの作成

---
+++ pages/todoTrash.tsx
@@ -0,0 +1,55 @@
+import { Box, Flex, Spacer, HStack, Center } from "@chakra-ui/react";
+import { BackButton } from "../components/atoms/buttons/BackButton";
+import { DeleteAllButton } from "../components/atoms/buttons/DeleteAllButton";
+import { RestoreAllButton } from "../components/atoms/buttons/RestoreAllButton";
+import { Header } from "../components/organisms/Header";
+import { TodoTrash } from "../components/organisms/Trash/TodoTrash";
+import { GreyButton, NumButtonFirst, NumButtonSecond, NumButtonPoint, NumButtonFifth, NumButtonSixth, WhiteButton } from "../components/templates/trash/NumButton";
+
+
+
+export const TodotrashPage = () => {
+  return (
+    <>
+      <Box>
+        <Header />
+
+        <Flex mb="39px">
+          <Box
+            maxW="92px"
+            minW="92px"
+            maxH="33px"
+            minH="33px"
+            mt="16px"
+            ml="100px"
+            fontSize="24px"
+            fontWeight="bold"
+          >
+            TRASH
+          </Box>
+          <Spacer />
+          <HStack spacing="16px" mr="93px" mt="24px">
+            <DeleteAllButton />
+            <RestoreAllButton />
+            <BackButton />
+          </HStack>
+        </Flex>
+        
+        <TodoTrash />
+
+        {/* Pagenation機能 実装後消去予定 */}
+        <Center mt="96px" mb="24px">
+        <HStack spacing="12px">
+          <GreyButton />
+          <NumButtonFirst />
+          <NumButtonSecond/>
+          <NumButtonPoint/>
+          <NumButtonFifth/>
+          <NumButtonSixth/>
+          <WhiteButton/>
+        </HStack>
+        </Center>
+      </Box>
+    </>
+  );
+};

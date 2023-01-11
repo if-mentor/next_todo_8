7363e203a10661fab411b40dbf45813b94df84ae
maki <murasame.asa.73@gmail.com>
2023-01-11 18:38:11 +0900

    #31 add: deleteAllボタンの機能実装

--- components/atoms/buttons/DeleteAllButton.tsx
+++ components/atoms/buttons/DeleteAllButton.tsx
@@ -1,6 +1,49 @@
+import React, { useEffect, useState } from 'react';
 import { Button } from "@chakra-ui/react";
+import { collection, CollectionReference, deleteDoc, doc, Firestore, onSnapshot, Timestamp } from "firebase/firestore";
+import { db } from '../../../firebase';
+
+type Todos = {
+  id: string;
+  createdAt: Timestamp;
+  detail: string;
+  priority: string;
+  status: string;
+  text: string;
+  updateAt: Timestamp;
+  trash: boolean;
+};
+
+// idの取得方法が判明したら変更↓
+const id ="daaxwBhgR5yWyZ9l1bKs"
+
 
 export const DeleteAllButton = () => {
+  const [todos, setTodos] = useState<Array<Todos>>([]);
+
+  useEffect(() => {
+    //データの取得
+    const postData = collection(db, 'todos') as CollectionReference<Todos>;
+    //リアルタイムでデータを取得する。
+    onSnapshot(postData, (todo) => {
+      setTodos(todo.docs.map((doc) => ({ ...doc.data() })));
+    });
+  }, []);
+
+  const [filterTodos, setFilterTodos] = useState<Array<Todos>>([...todos]);
+  useEffect(() => {
+    setFilterTodos(todos.filter((todo:any) => {
+      return todo.trash === true
+    }))
+  }, [todos])
+
+  // idを取得方法が判明したら編集
+  // firebaseから削除する関数
+  const handleDeleteAllTodo = ()=>{
+    filterTodos.map((id) => 
+    deleteDoc(doc(db, "todos",id.id))
+  )}
+
   return (
     <Button
       _hover={{ bg: "#E28F84" }}
@@ -11,6 +54,7 @@ export const DeleteAllButton = () => {
       w={"112px"}
       fontSize={"18px"}
       fontWeight={'bold'}
+      onClick={() => handleDeleteAllTodo(id)}
     >
       Delete All
     </Button>

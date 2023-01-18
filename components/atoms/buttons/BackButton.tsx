0a30ad500e6fef1c361abee44efb0eee7484f71b
maki <murasame.asa.73@gmail.com>
2023-01-18 10:50:40 +0900

    #37 add: todoTopページへのリンク機能を実装

--- components/atoms/buttons/BackButton.tsx
+++ components/atoms/buttons/BackButton.tsx
@@ -1,18 +1,21 @@
 import { Button } from "@chakra-ui/react";
+import Link from "next/link";
 
 export const BackButton = () => {
   return (
-    <Button
-      _hover={{ bg: "#95E3F4" }}
-      backgroundColor={"#95E3F4"}
-      rounded="full"
-      border="1px"
-      color={"blackAlpha.800"}
-      w={"104px"}
-      fontSize={"18px"}
-      fontWeight={"bold"}
-    >
-      Back
-    </Button>
+    <Link href={"/todoTop"}>
+      <Button
+        _hover={{ bg: "#95E3F4" }}
+        backgroundColor={"#95E3F4"}
+        rounded="full"
+        border="1px"
+        color={"blackAlpha.800"}
+        w={"104px"}
+        fontSize={"18px"}
+        fontWeight={"bold"}
+      >
+        Back
+      </Button>
+    </Link>
   );
 };

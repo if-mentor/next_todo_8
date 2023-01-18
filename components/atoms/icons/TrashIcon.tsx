fb5a579e1146a0c3cd84ad6543dfe8391e79241d
maki <murasame.asa.73@gmail.com>
2023-01-18 09:37:20 +0900

    #36 add: TrashIconをクリックしたときにtodoTrashページに遷移する機能を実装

--- components/atoms/icons/TrashIcon.tsx
+++ components/atoms/icons/TrashIcon.tsx
@@ -1,26 +1,29 @@
 import { Box, Icon } from '@chakra-ui/react'
+import Link from 'next/link'
 import React from 'react'
 import { FaTrashRestoreAlt } from 'react-icons/fa'
 
 const TrashIcon = () => {
   return (
+    <Link href={"/todoTrash"}>
       <Box
-      display='flex'
-      justifyContent='center'
-      alignItems='center'
-      w='40px'
-      h='40px'
-      border='1px solid #B5B5B5'
-      borderRadius='50%'
-      backgroundColor='#F6E05E'
-    >
-      <Icon 
-        as={FaTrashRestoreAlt}
-        w='14px' 
-        h='18px'
-        cursor="pointer"
-      />
-    </Box>
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+        w='40px'
+        h='40px'
+        border='1px solid #B5B5B5'
+        borderRadius='50%'
+        backgroundColor='#F6E05E'
+      >
+        <Icon 
+          as={FaTrashRestoreAlt}
+          w='14px' 
+          h='18px'
+          cursor="pointer"
+        />
+      </Box>
+    </Link>
   )
 }
 
